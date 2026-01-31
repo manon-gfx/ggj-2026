d1f7973eab32748b3cf9b2f54291cf7c31db6211
Anne Poot <91520388+annepoot@users.noreply.github.com>
2026-01-31 14:22:34 +0100

    📈 Implemented sine wave (#39)

--- src/audio/sound.rs
+++ src/audio/sound.rs
@@ -1,4 +1,5 @@
 use crate::{audio::notes::*, game::Key};
+use interp::{InterpMode, interp};
 
 struct MusicSettings;
 
@@ -49,4 +50,44 @@ pub fn square_wave(t: f64, freq: f64) -> f64 {
 
 pub fn sawtooth_wave(t: f64, freq: f64) -> f64 {
     2. * (freq * t) % 2. - 1.
+}
+
+pub fn custom_wave(t: f64, freq: f64, ttable: &[f64], ytable: &[f64]) -> f64 {
+    let t_rel = (freq * t) % 1.;
+    interp(&ttable, &ytable, t_rel, &InterpMode::default())
+}
+
+pub const TRIANGLETTABLE: [f64; 5] = [0.0, 0.25, 0.50, 0.75, 1.0];
+pub const TRIANGLEYTABLE: [f64; 5] = [0.0, 1.0, 0.0, -1.0, 0.0];
+
+pub const SINETTABLE: [f64; 21] = [
+    0.0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75,
+    0.80, 0.85, 0.90, 0.95, 1.0,
+];
+pub const SINEYTABLE: [f64; 21] = [
+    0.0000000000000000,
+    0.3090169883750000,
+    0.5877852522920000,
+    0.8090169883750000,
+    0.9510565162950000,
+    1.0000000000000000,
+    0.9510565162950000,
+    0.8090169883750000,
+    0.5877852522920000,
+    0.3090169883750000,
+    0.0000000000000000,
+    -0.3090169883750000,
+    -0.5877852522920000,
+    -0.8090169883750000,
+    -0.9510565162950000,
+    -1.0000000000000000,
+    -0.9510565162950000,
+    -0.8090169883750000,
+    -0.5877852522920000,
+    -0.3090169883750000,
+    0.0000000000000000,
+];
+
+pub fn sine_wave(t: f64, freq: f64) -> f64 {
+    custom_wave(t, freq, &SINETTABLE, &SINEYTABLE)
 }
